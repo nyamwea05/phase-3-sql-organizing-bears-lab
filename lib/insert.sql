eba433ce2b95d95db9153b12e1feeb4366547d95
sandra <nyamweasandra@gmail.com>
2023-05-25 10:15:17 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+ (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+  (2, 'Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  (3, 'Tabitha', 4, 'F', 'Gray', 'Playful', 1),
+  (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Brave', 1),
+  (5, 'Melissa', 2, 'F', 'Golden', 'Sweet', 1),
+  (6, 'Grinch', 7, 'M', 'Green', 'Mischievous', 1),
+  (7, 'Wendy', 5, 'F', 'White', 'Friendly', 1),
+  (8, 'unnamed', NULL, NULL, NULL, NULL, NULL);
